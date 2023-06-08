45885fb5e94941653b5e7a7729f4a7f403d0d89a
Plat <git@p1at.dev>
2023-06-08 10:27:00 +0900

    feat: simple cli script

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-import setuptools
+from setuptools import setup, find_packages
 
-# install src dir
-setuptools.setup(
-    name="src",
-    version="0.0.0",
-    packages=setuptools.find_packages(where="src"),
-    package_dir={"": "src"},
-)
+
+setup(name="src", packages=find_packages())
--- src/utils/zip.py
+++ src/utils/zip.py
@@ -2,11 +2,15 @@ import zipfile
 from typing import List
 from pathlib import Path
 
+from tqdm import tqdm
+
 
 def save_as_zip(buffers: List[bytes], path_to_save: str | Path, file_ext: str = ".png"):
     with zipfile.ZipFile(path_to_save, "w") as zip_file:
-        for index, buf in enumerate(buffers):
-            # ZIPにバッファから画像を書き込む
-            data = zipfile.ZipInfo(f"{index}{file_ext}")
-            data.compress_type = zipfile.ZIP_DEFLATED
-            zip_file.writestr(data, buf)
+        with tqdm(total=len(buffers)) as pbar:
+            for index, buf in enumerate(buffers):
+                # ZIPにバッファから画像を書き込む
+                data = zipfile.ZipInfo(f"{index}{file_ext}")
+                data.compress_type = zipfile.ZIP_DEFLATED
+                zip_file.writestr(data, buf)
+                pbar.update(1)
